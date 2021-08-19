7ec120fb44bd9e01fe3f3e7eea389110bd55745f
djakesh <75931918+djakesh@users.noreply.github.com>
2021-08-19 17:37:52 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -17,6 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'whitenoise.runserver_nostatic',
 
     'crispy_forms',
     
@@ -33,6 +35,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
